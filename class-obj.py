8529016ce4851f39b96b15553be687f29f3473a2
vishal-viji <vishalshanmugam928@gmail.com>
2026-09-11 13:05:39 +0530

    oop

--- class-obj.py
+++ class-obj.py
@@ -9,6 +9,9 @@ class Human:
     
     def greet(self):
         print("good evening...")
+
+
+# objects
         
 obj1=Human()
 obj1.humandetails()
@@ -20,3 +23,11 @@ obj2.age=20
 obj2.category=obj2.category[0]
 obj2.humandetails()
 obj2.greet()
+
+
+obj3=Human()
+obj3.human_name="Suba"
+obj3.age=18
+obj3.category=obj3.category[1]
+obj3.humandetails()
+obj3.greet()
