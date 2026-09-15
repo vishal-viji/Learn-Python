687a9d53e00782bdc77b9e495aced8c4da220890
vishal-viji <vishalshanmugam928@gmail.com>
2026-09-15 12:14:17 +0530

    pytohmn

--- multilevel-inheritaance.py
+++ multilevel-inheritaance.py
@@ -11,10 +11,6 @@ class company():
 class Employee(company):
     def __init__(self, name, age, salary, companyname, location, branch):
         super().__init__(companyname, location, branch)
-        # self.name = name
-        # self.location = location
-        # self.branch = branch
-        super().__init__(companyname, location, branch)
         self.age = age
         self.salary = salary
         self.company = companyname
@@ -23,10 +19,20 @@ class Employee(company):
         
     def employeedetails(self):
         return f"\nEmployee Age: {self.age}\nEmployee Salary: {self.salary}\ncompany name is {self.company}"
-
-        
-        
-emp1=Employee("Vishal", 20, 20000, "TCS", "Bangalore", "IT")
-print(emp1.employeedetails())
-        
     
+class manager(Employee): #sub child class
+    def __init__(self, name, age, salary, companyname, location, branch, department,managername,deliverystream):
+        super().__init__(name, age, salary, companyname, location, branch)
+        self.department = department
+        self.managername = managername
+        self.deliverystream = deliverystream
+        print("i have set the values")
+    def managerdetails(self):
+        return f"\nEmployee Age: {self.age}\nEmployee Salary: {self.salary}\ncompany name is {self.company}\nDepartment: {self.department}\nManager Name: {self.managername}\nDelivery Stream: {self.deliverystream}"
+
+    
+obj1=manager("Vishal", 20, 20000, "TCS", "Bangalore", "IT", "IT", "Ramesh", "Delivery")
+print(obj1.managerdetails())
+print(obj1.companydetails())
+print(obj1.employeedetails())
+        
