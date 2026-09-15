d6ceffd6fdb0e918951d9cfc88043aa3ff8c5fdf
vishal-viji <vishalshanmugam928@gmail.com>
2026-09-15 12:12:06 +0530

    python

--- multilevel-inheritaance.py
+++ multilevel-inheritaance.py
@@ -11,6 +11,10 @@ class company():
 class Employee(company):
     def __init__(self, name, age, salary, companyname, location, branch):
         super().__init__(companyname, location, branch)
+        # self.name = name
+        # self.location = location
+        # self.branch = branch
+        super().__init__(companyname, location, branch)
         self.age = age
         self.salary = salary
         self.company = companyname
@@ -19,21 +23,10 @@ class Employee(company):
         
     def employeedetails(self):
         return f"\nEmployee Age: {self.age}\nEmployee Salary: {self.salary}\ncompany name is {self.company}"
-    
-class manager(Employee): #sub child class
-    def __init__(self, name, age, salary, companyname, location, branch, department,managername,deliverystream):
-        super().__init__(name, age, salary, companyname, location, branch)
-        self.department = department
-        self.managername = managername
-        self.deliverystream = deliverystream
-        print("i have set the values")
-    def managerdetails(self):
-        return f"\nEmployee Age: {self.age}\nEmployee Salary: {self.salary}\ncompany name is {self.company}\nDepartment: {self.department}\nManager Name: {self.managername}\nDelivery Stream: {self.deliverystream}"
 
-    
-obj1=manager("Vishal", 20, 20000, "TCS", "Bangalore", "IT", "IT", "Ramesh", "Delivery")
-print(obj1.managerdetails())
-print(obj1.companydetails())
-print(obj1.employeedetails())
+        
+        
+emp1=Employee("Vishal", 20, 20000, "TCS", "Bangalore", "IT")
+print(emp1.employeedetails())
         
     
